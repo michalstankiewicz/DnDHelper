ef9714ae3b66def01f15aab2449b456c1d8e56c1
michalstankiewicz <maveru@gmail.com>
2026-04-08 00:50:04 +0200

    adding todo comment to test CI

--- dice.py
+++ dice.py
@@ -3,3 +3,5 @@ import random
 
 def roll_dice(sides):
     return random.randint(1, sides)
+
+# todo: add more dice types (d4, d6, d8, d10, d12, d20) and a function to roll multiple dice at once (e.g., 2d6)
