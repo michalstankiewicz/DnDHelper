fd4b13d7f4529feee2b4aaec267013f041c31d0b
michalstankiewicz <maveru@gmail.com>
2026-04-08 01:05:23 +0200

    adding --test in main.py  not be trapped in infinity loop.

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import gui
-
+# added for testing purposes, to check if the application starts without errors. Run "python main.py --test" to execute this test.
+import sys
+if "--test" in sys.argv:
+    print("Breaking in so we can break out")
+    exit(0)
 
 # this is the main file which will start whole program.
 if __name__ == "__main__":
